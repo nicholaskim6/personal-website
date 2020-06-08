e31aad7f85623121003b9c14f7577cb578d0ccf8
Nick Kim <nicholastkim@gmail.com>
2020-06-08 04:22:08 -0400

    new project

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,9 +1,17 @@
 // TODO Add a couple lines about each project
 const data = [
   {
+    title: 'Financial Planning Portfolio Comparison (ORF435)',
+    subtitle: '',
+    link: 'https://github.com/nicholaskim6/financial-planning-ORF435',
+    image: 'https://raw.githubusercontent.com/nicholaskim6/personal-website/master/public/images/projects/financial-planning.PNG',
+    date: '2019-12',
+    desc: 'Portfolio comparison for achieving hypothetical down payment and retirement goals'
+  },
+  {
     title: 'Auto-ble',
     subtitle: '',
-    link: 'https://github.com/JulianKnodt/hackprincetonSpring2018',
+    link: 'https://github.com/nicholaskim6/hackprincetonSpring2018',
     image: 'https://raw.githubusercontent.com/nicholaskim6/personal-website/master/public/images/projects/Autoble.PNG',
     date: '2018-3',
     desc: 'Automatic generation of music. Supervised Learning approach with RNNs and preprocessed chords'
